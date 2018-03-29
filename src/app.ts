3c48e72d6f7bfb7550b661188411e611464377b7
deven <sathedeven@gmail.com>
2018-03-29 18:42:12 +0530

    added test code

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import {Router, RouterConfiguration} from 'aurelia-router';
+    import {PLATFORM} from 'aurelia-pal';
+
+    export class App {
+      router: Router;
+
+      configureRouter(config: RouterConfiguration, router: Router){
+        config.title = 'Contacts';
+        config.map([
+          { route: '',              moduleId: PLATFORM.moduleName('no-selection'),   title: 'Select' },
+          { route: 'contacts/:id',  moduleId: PLATFORM.moduleName('contact-detail'), name:'contacts' }
+        ]);
+
+        this.router = router;
+      }
+    }
+  
